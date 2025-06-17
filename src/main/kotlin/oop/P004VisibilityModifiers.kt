a8292a569d08fb972ab9cfd6c00f7261eb1a36ed
Kaustubh <162901396+deshkaustubh@users.noreply.github.com>
2025-06-17 20:29:00 +0530

    kotlin visibility modifiers

---
+++ src/main/kotlin/oop/P004VisibilityModifiers.kt
@@ -0,0 +1,13 @@
+package org.example.oop
+
+/*
+4 types ->
+they are -
+    Public - Default , cab be accessed everywhere,
+    private - Available only inside the containing file or class
+    protected - Same as private but available inside subclasses or child classes
+
+         internal -> Available everywhere inside the same module
+ */
+
+
