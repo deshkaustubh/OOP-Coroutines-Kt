f9ffc7429abe86335fe46a2322f2fabbf7c32d32
Kaustubh <162901396+deshkaustubh@users.noreply.github.com>
2025-06-17 14:43:12 +0530

    properties and methods

--- src/main/kotlin/oop/P002ClassesAndObjects.kt
+++ src/main/kotlin/oop/P002ClassesAndObjects.kt
@@ -1,5 +1,22 @@
 package org.example.oop
 
 fun main() {
+    var i  = 20
+    println(i.plus(30))
+    println(i.toFloat())
 
+    val P1 = Person("A", 20)
+    val P2 = Person("B", 14)
+
+    println(P1.canVote())
+    println(P2.canVote())
+}
+
+// classes can be used to crate your own data type like car or account
+
+class Person(val name: String, var age: Int) { // properties
+    //methods
+    fun canVote(): Boolean {
+        return age > 18
+    }
 }
